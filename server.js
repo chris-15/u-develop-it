37334d0ab0f0756fbbe3c6a364ded95fa146ef10
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-04-05 15:41:23 -0400

    added more db queries

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -19,8 +20,36 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 )
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
+/* db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
+}); */
+
+//GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+//Delet a candidate
+/* db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(result);
+}); */
+
+//create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES(?, ?, ?, ?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 
